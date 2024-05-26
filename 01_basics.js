9ce8c2de3e36ad31565e9fc0f90094eb4682c78c
RAHUL MANDAL <rahulmandal99555@gmail.com>
2024-05-26 14:23:44 +0530

    Git HUB for first time

---
+++ 01_basics.js
@@ -0,0 +1,55 @@
+// //Data Types
+// let name= "Rahul Mandal"
+// let age = 25
+// let indianNationality = true
+// let phoneNumber = 7667366525
+
+console.log("Hi everyone hi this is this is a project on JS"); less
+// //Data Types
+// let name= "Rahul Mandal"
+// let age = 25
+// let indianNationality = true
+// let phoneNumber = 7667366525
+
+console.log("Hi everyone hi this is this is a project on JS");
+// //Data Types
+// let name= "Rahul Mandal"
+// let age = 25
+// let indianNationality = true
+// let phoneNumber = 7667366525
+
+console.log("Hi everyone hi this is this is a project on JS");
+// //Data Types
+// let name= "Rahul Mandal"
+// let age = 25
+// let indianNationality = true
+// let phoneNumber = 7667366525
+
+console.log("Hi everyone hi this is this is a project on JS");// //Data Types
+// let name= "Rahul Mandal"
+// let age = 25
+// let indianNationality = true
+// let phoneNumber = 7667366525
+
+console.log("Hi everyone hi this is this is a project on JS");
+// //Data Types
+// let name= "Rahul Mandal"
+// let age = 25
+// let indianNationality = true
+// let phoneNumber = 7667366525
+
+console.log("Hi everyone hi this is this is a project on JS");
+// //Data Types
+// let name= "Rahul Mandal"
+// let age = 25
+// let indianNationality = true
+// let phoneNumber = 7667366525
+
+console.log("Hi everyone hi this is this is a project on JS");
+// //Data Types
+// let name= "Rahul Mandal"
+// let age = 25
+// let indianNationality = true
+// let phoneNumber = 7667366525
+
+console.log("Hi everyone hi this is this is a project on JS");
---
+++ movies/02_basics.js
@@ -0,0 +1,7 @@
+// //Data Types
+// let name= "Rahul Mandal"
+// let age = 25
+// let indianNationality = true
+// let phoneNumber = 7667366525
+
+console.log("Hi everyone this is a project on JS");
