aa4b77615feac493ee1b45138e9da6f1d9e6443f
RAHUL MANDAL <43607673+Rahul1181@users.noreply.github.com>
2025-12-24 09:02:46 +0530

    Delete movies directory

--- movies/02_basics.js
+++
@@ -1,7 +0,0 @@
-// //Data Types
-// let name= "Rahul Mandal"
-// let age = 25
-// let indianNationality = true
-// let phoneNumber = 7667366525
-
-console.log("Hi everyone this is a project on JS");
